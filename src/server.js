8d93de3e84fac7b1b69c7691002148883665e9b3
una <oksana2020la@gmail.com>
2024-06-11 08:58:35 -0700

    update

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ export const setupServer = () => {
       const contacts = await getAllContacts();
 
       res.status(200).json({
-        status: 'success',
+        status: '200',
         message: 'Successfully found contacts!',
         data: contacts,
       });
@@ -44,7 +44,7 @@ export const setupServer = () => {
       }
 
       res.status(200).json({
-        status: 'success',
+        status: '200',
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
